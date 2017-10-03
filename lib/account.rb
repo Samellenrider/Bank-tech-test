80821905e171cc5ce688ab08cf4a438ceba653a0
Sam Ellenrieder <sam.ell@web.de>
2017-10-03 16:09:33 +0100

    fifth commit, refactoring, solving prinitng problem

--- lib/account.rb
+++ lib/account.rb
@@ -1,64 +1,47 @@
-require 'date'
+require 'statement'
 
 class Account
-  attr_reader :name, :date # , :statement
-  attr_accessor :credit, :transactions, :debit
+  attr_reader :name, :date
+  attr_accessor :credit, :transactions, :debit, :topup
 
   def initialize(name, date = Time.now)
     @name = name
     @credit = 0
     @debit = 0
-    # @statement = Statement.new
+    @topup = 0
     @transactions = []
     @date = date.to_s
   end
 
-  def balance
-    p @credit
-  end
 
   def topup(amount)
-    @credit += amount
-    t = { date: @date, name: @name, credit: @credit, debit: @debit }
-    @transactions.push(t)
-  end
-
-  def account
-    p @name
-    p @credit
+    @credit += amount    
+    @transactions << {date: @date, name: @name, credit: @credit, debit: @debit, topup: amount}
+    "You topped up $#{amount}. New balance $#{@credit}"
   end
 
   def withdrawl(amount)
     if @credit >= amount
       @credit -= amount
-      t = { date: @date, name: @name, credit: @credit, debit: amount }
-      @transactions.push(t)
-      p "You withdrawled $#{amount}. New balace $#{@credit}."
+      @transactions << {date: @date, name: @name, credit: @credit, debit: amount, topup: @topup}
+       "You withdrawled $#{amount}. New balace $#{@credit}."
     else
-      p 'You have insufficient funds.'
+       "You have insufficient funds."
     end
   end
 
-  # def log
-  #   single_transaction = { date: @date,
-  #                          name: @name,
-  #                          credit: @credit,
-  #                          debit: @debit }
-  #   #transactions = []
-  #   @transactions.push(single_transaction)
-  # end
-
   def print_statement
-    @transactions.each do |name, date, credit, debit|
-      print "#{name}||#{date}||#{credit}||#{debit}"
-
-      #    if debit == 0
-      #     print "name || date || credit || debit ""\n"
-      # else
-      # 	print "name || date || credit || debit ""\n"
-      #   end
-      # print "name || date || credit || debit\n"
-      # puts @transactions
+  	print "name || date || credit || debit || topup \n"
+    @transactions.each do |elt|
+    		elt.each do |key, value|
+    		  "#{key}||"#{value}||#{value}||#{value}||#{value}"
+    		end
+    	end
     end
- end
-end
+  end
+end	
+
+ac =Account.new("Sam")
+ac.topup(100)
+ac.withdrawl(50)
+ac.print_statement
